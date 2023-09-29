8a739decc03945858af5c672f30a2639667a593b
PayamAnari <anari.p62@gmail.com>
2023-09-29 15:50:34 +0200

    Add prep-week3

---
+++ week3/check-screen-size/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ScreenSize from './component/ScreenSize';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Screen Size Example</h1>
+      <ScreenSize />
+    </div>
+  );
+}
+
+export default App;
