9d40fb2a14f78fb0525ebb467a3a6253f30c78eb
PayamAnari <anari.p62@gmail.com>
2023-09-29 15:54:32 +0200

    Add usescreensize.js

---
+++ week3/check-screen-size/src/hook/useScreenSize.js
@@ -0,0 +1,30 @@
+import { useState, useEffect, useDebugValue } from 'react';
+
+const useScreenSize = () => {
+  const [screenSize, setScreenSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setScreenSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  // Provide a label for React DevTools
+  useDebugValue('Screen Size');
+
+  return screenSize;
+};
+
+export default useScreenSize;
